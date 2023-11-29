08dd10d111356866f68eedb02e71cbd1f14151a5
Heman Sakkthivel <sakkthivelheman@gmail.com>
2023-11-29 18:51:21 +0530

    generated walls -> 0

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
-#include <SDL.h>
+// #include <SDL.h>
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int> > generateMaze(int width, int height) {
+    std::vector<std::vector<int> > mazee(width, std::vector<int>(height, 0));
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            mazee[i][j]=0;
+        }
+        
+    }
+    return mazee;
+}
 int main() {
-    // game logic goes here
+    const int width=16, height=16;
+    std::vector<std::vector<int> > maze = generateMaze(width, height);
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            std::cout << maze[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
